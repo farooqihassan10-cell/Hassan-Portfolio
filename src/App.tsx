6db51bdd9f5a50620710a2afb0aa90d08c55ce2b
Hassan Farooqi <farooqihassan10@gmail.com>
2026-08-04 03:10:41 +0500

    Fix casing of interactiveParticles component

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,7 @@ export default function App() {
             <Projects />
             <DesignGallery />
             <WebDesigns />
-            <interactiveparticles />
+            <interactiveParticles />
             <Services />
             <DriveHub />
             <Testimonials />
